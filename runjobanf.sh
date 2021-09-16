4859c5f5c207c1b8123cd4e8b3d6c4b81e657552
iwillsky <45908192+Iwillsky@users.noreply.github.com>
2021-09-16 21:56:30 +0800

    Update0916

--- runjobanf.sh
+++ runjobanf.sh
@@ -6,7 +6,7 @@
 #SBATCH --mem=96G
 #SBATCH --gres=gpu:1
 JOBID=$1 #$SLURM_ARRAY_JOB_ID
-WORKDIR=/shared/home/cycleadmin/RoseTTAFold
+WORKDIR=/volanf/RoseTTAFold
 
 $WORKDIR/run_pyrosetta_hpc.sh $WORKDIR/input.fa $WORKDIR $JOBID 16 96
 #$WORKDIR/run_e2e_hpc.sh $WORKDIR/input.fa $WORKDIR $JOBID 16 96
